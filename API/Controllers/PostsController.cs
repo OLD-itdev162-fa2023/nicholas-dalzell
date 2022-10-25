a9beae0c73aea5342508be48f9a61cd40f84685a
Nick Dalzell <dalzelln@gmatc.matc.edu>
2022-10-25 18:39:59 -0500

    working on activity 6

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -24,5 +24,45 @@ namespace API.Controllers
         {
             return this.context.Posts.ToList();
         }
+
+        /// <summary>
+        /// GET api/post/[id]
+        /// </summary>
+        /// <param name = "id">Post id</param>
+        /// <returns> A list of posts </returns>
+        [HttpGet("{id}", Name = "GetById")]
+        public ActionResult<Post> GetById(Guid id) {
+            var post = this.context.Posts.Find(id);
+            if (post is null) {
+                return NotFound();
+            }
+
+            return Ok(post);
+        }
+
+        ///<summary>
+        /// POST api/post
+        ///</summary>
+        ///<param name = "request">JSON request containing post fields</param>
+        ///<returns>A new post</returns>
+        [HttpPost(Name = "Create")]
+        public ActionResult<Post> Create([FromBody]Post request) {
+            var post = new Post {
+                Id = request.Id,
+                Title = request.Title,
+                Body = request.Body,
+                Date = request.Date
+            };
+
+            context.Posts.Add(post);
+            var success = context.SaveChanges() > 0;
+
+            if (success) {
+                return Ok(post);
+            }
+
+            throw new Exception("Error creating post");
+        }
     }
 }
+
